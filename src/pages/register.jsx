8577bca191c7c5d9a91312d086694dad62b5cd86
yeheskhielprasetyo <yeheskhielprasetyorakordana123@gmail.com>
2024-03-16 18:59:07 +0800

    learn react router

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,15 @@
 import AuthLayout from "../components/Layouts/AuthLayouts"
 import FormRegister from "../components/Fragments/FormRegister";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
     return (
         <AuthLayout title="Register">
             <FormRegister/>
+            <p className="text-sm mt-5 text-center">
+                Have have an account ? 
+                <Link to="/login" className="font-bold text-blue-600">Login</Link>
+            </p>
         </AuthLayout>
     )
 }
